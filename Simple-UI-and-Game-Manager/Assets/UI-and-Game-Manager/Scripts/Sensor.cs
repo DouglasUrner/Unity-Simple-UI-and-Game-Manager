d8b72a94b09169076e18c0787998c50a644bb791
Douglas Urner <dlu@canishe.com>
2023-02-01 21:55:59 -0800

    Add score & health support to sensor.

--- Simple-UI-and-Game-Manager/Assets/UI-and-Game-Manager/Scripts/Sensor.cs
+++ Simple-UI-and-Game-Manager/Assets/UI-and-Game-Manager/Scripts/Sensor.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class Sensor : MonoBehaviour
 {
+  // What to do on a collision.
+  public enum CollisionAction
+  {
+    endGame,
+    addPoints,
+    subPoints,
+    addHealth,
+    subHealth
+  }
 
+  public enum CollisionEvent
+  {
+    onEnter,
+    onExit
+  }
+  public CollisionAction action = CollisionAction.endGame;
+  public int amount = 1;
+  public CollisionEvent collisionEvent = CollisionEvent.onEnter;
   private GameManager gameManager;
 
   // Start is called before the first frame update
@@ -15,6 +32,50 @@ public class Sensor : MonoBehaviour
 
   void OnTriggerEnter()
   {
-    gameManager.gameEnding = true;
+    if (collisionEvent == CollisionEvent.onEnter)
+    {
+      TriggerAction();
+    }
+    else
+    {
+      return;
+    }
+  }
+
+  void OnTriggerExit()
+  {
+    if (collisionEvent == CollisionEvent.onExit)
+    {
+      TriggerAction();
+    }
+    else
+    {
+      return;
+    }
+  }
+
+  void TriggerAction()
+  {
+    switch (action)
+    {
+      case CollisionAction.endGame:
+        gameManager.gameEnding = true;
+        break;
+      case CollisionAction.addPoints:
+        gameManager.AddPoints(amount);
+        break;
+      case CollisionAction.subPoints:
+        gameManager.AddPoints(-amount);
+        break;
+      case CollisionAction.addHealth:
+        gameManager.IncreaseHealth(amount);
+        break;
+      case CollisionAction.subHealth:
+        gameManager.DecreaseHealth(amount);
+        break;
+      default:
+        Debug.Log($"unhandled CollisionAction: {action}");
+        break;
+    }
   }
 }
