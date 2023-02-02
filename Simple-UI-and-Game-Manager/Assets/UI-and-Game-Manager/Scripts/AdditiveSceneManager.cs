610303bcc45ef856e81ea787603fe4a16b3dd43a
Douglas Urner <dlu@canishe.com>
2023-02-01 22:43:06 -0800

    Attempt to ensure that the scene at index 1 remains active.

--- Simple-UI-and-Game-Manager/Assets/UI-and-Game-Manager/Scripts/AdditiveSceneManager.cs
+++ Simple-UI-and-Game-Manager/Assets/UI-and-Game-Manager/Scripts/AdditiveSceneManager.cs
@@ -13,6 +13,19 @@ public class AdditiveSceneManager : MonoBehaviour
       // that the UI-and-Game-Manager scene is at index 0, other
       // scenes are at higher indexes. This is kind of a hack... (XXX)
       SceneManager.LoadScene(1, LoadSceneMode.Additive);
+      var sceneName = GetSceneNameByIndex(1);
+      SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
     #endif
+  }
+
+  static string GetSceneNameByIndex(int buildIndex)
+  {
+    if (buildIndex >= SceneManager.sceneCountInBuildSettings)
+    {
+      Debug.LogErrorFormat($"buildIndex ({buildIndex}) out of bounds");
+      return null;
+    }
+    var scene = SceneManager.GetSceneByBuildIndex(buildIndex);
+    return scene.name;
   }
 }
